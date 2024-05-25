41665c9e39c737e1914c1449a33cafb8bb104e09
YvanGB <yvangbaffonou@gmail.com>
2024-05-25 18:19:42 +0000

    changes in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const DBConnection = require('./src/db/dbconnection');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
